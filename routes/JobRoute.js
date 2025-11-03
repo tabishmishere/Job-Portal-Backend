4cac0475f60d3ce8e487588a332bdc5aec362b6b
tabishmishere <tabishm484@gmail.com>
2025-11-03 22:00:37 +0500

    improve the update logic in job update controller and also add routes properly

--- routes/JobRoute.js
+++ routes/JobRoute.js
@@ -1,12 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { getAllJobs, createJob,updateJob,deleteJob } from "../controllers/JobController.js";
+import { getAllJobs, createJob, updateJob, deleteJob } from "../controllers/JobController.js";
 
 router.get('/', getAllJobs);
 router.post('/', createJob);
 router.put('/:jobId', updateJob);
 router.delete('/:jobId', deleteJob);
-
-
 
 export default router;
