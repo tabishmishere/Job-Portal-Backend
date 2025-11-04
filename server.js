29e4b1877eda5a338137269fd8e127364d46f7c5
tabishmishere <tabishm484@gmail.com>
2025-11-04 21:31:09 +0500

    do some changes in server.js file

--- server.js
+++ server.js
@@ -9,26 +9,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],// frontend URL
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
 
-// 👇 Route Prefix must match your frontend fetch URL
 app.use("/api", userRoutes);
-
 app.use("/api/jobs", jobRoutes);
-
-// Error handling (optional)
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Something went wrong!" });
-});
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`RUNNING PORT: ${PORT}`));
